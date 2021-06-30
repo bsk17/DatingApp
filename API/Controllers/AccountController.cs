d6a8686e263794b2f21ca1cd48f637bea07b3fc0
bsk17 <sk.bijin@gmail.com>
2021-06-30 15:54:17 +0530

    End Of Section 4

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,91 @@
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using API.Data;
+using API.DTOs;
+using API.Entities;
+using API.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+             public class AccountController : BaseApiController
+             {
+                          private readonly DataContext _context;
+                          private readonly ITokenService _tokenService;
+                          public AccountController(DataContext context, ITokenService tokenService)
+                          {
+                                       _tokenService = tokenService;
+                                       _context = context;
+                          }
+
+                          [HttpPost("register")]
+                          // Here we are using DTO beacuse we have pass string objects (username and pasword)
+                          // if we simply mention strin username and string password it will not execute
+                          // inside Register the Username and Password are objects which can be used to execute this method
+
+                          // We can add "validations" at three places - DB, Entity, DTO
+                          // the DTO seems to be the best approach of all
+                          public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
+                          {
+                                       if (await UserExists(registerDto.Username))
+                                                    return BadRequest("Username already taken!!");
+
+                                       using var hmac = new HMACSHA512();
+
+                                       var user = new AppUser
+                                       {
+                                                    UserName = registerDto.Username.ToLower(),
+                                                    PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
+                                                    // here a random key is generated always
+                                                    PasswordSalt = hmac.Key
+                                       };
+
+                                       _context.Users.Add(user);
+                                       await _context.SaveChangesAsync();
+
+                                       return new UserDto
+                                       {
+                                                    Username = user.UserName,
+                                                    Token = _tokenService.CreateToken(user)
+                                       };
+                          }
+
+                          [HttpPost("login")]
+                          public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+                          {
+                                       var user = await _context.Users
+                                                .SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
+
+                                       if (user == null)
+                                                    return Unauthorized("Invalid Username!!!");
+
+                                       // we are providing this argument to avoid generating random key 
+                                       // which would then compute a different mac
+                                       // the already generated key is the password salt 
+                                       // which we have saved during the time of registration
+                                       using var hmac = new HMACSHA512(user.PasswordSalt);
+
+                                       var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+                                       // check each character of the password
+                                       for (int i = 0; i < computedHash.Length; i++)
+                                       {
+                                                    if (computedHash[i] != user.PasswordHash[i])
+                                                                 return Unauthorized("Invalid Password !!");
+                                       }
+
+                                       return new UserDto
+                                       {
+                                                    Username = user.UserName,
+                                                    Token = _tokenService.CreateToken(user)
+                                       };
+                          }
+
+                          private async Task<bool> UserExists(string username)
+                          {
+                                       return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
+                          }
+             }
+}
